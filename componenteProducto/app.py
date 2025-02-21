80eabd07559c1bef7debe1bd1876e20f176358c8
Stevenhr <estevenhr@hotmail.com>
2025-02-21 17:14:58 -0500

    Unificar ramas

--- componenteProducto/app.py
+++ componenteProducto/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import Response
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
 from models.products import Product
 from controllers.product_ctr import product_ctr
 from db import init_db
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +19,10 @@ swagger_ui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={"ap
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 app.register_blueprint(product_ctr, url_prefix='/api')
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
